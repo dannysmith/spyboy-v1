e2d97c3835ac9e636909bfabd1fb4edc7d028588
Danny Smith <danny@dasmith.co.uk>
2012-05-16 17:55:00 +0100

    Added Image uploads

--- Gemfile
+++ Gemfile
@@ -4,5 +4,8 @@ gem 'sinatra', '~>1.3.2'
 gem 'data_mapper', '~>1.2.0'
 gem 'dm-postgres-adapter', :group => :production
 gem 'dm-sqlite-adapter', :group => :development
+gem "fog", "~> 1.3.1"
+gem 'mini_magick', '~>3.4'
 gem 'carrierwave', '~>0.6.2'
 gem 'carrierwave-datamapper', :require => 'carrierwave/datamapper'
+gem 'sinatra-flash', '~>0.3.0', :require => 'sinatra/flash'
--- spyboy.rb
+++ spyboy.rb
@@ -3,12 +3,50 @@ require "bundler/setup"
 require "sinatra/base"
 require 'rubygems'
 require 'data_mapper'
+require 'carrierwave'
 require 'carrierwave/datamapper'
+require 'sinatra/flash'
+
+####################### CARRIERWAVE SETUP ##########################
+
+CarrierWave.configure do |config|  
+  config.root = "#{Dir.pwd}/public"
+# Set up Carrierwave - Production
+#   config.fog_credentials = {
+#     :provider               => 'AWS',       # required
+#     :aws_access_key_id      => 'XXX',       # required
+#     :aws_secret_access_key  => 'yyy',       # required
+#     :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+#   }
+#   config.fog_directory  = 'name_of_directory'                     # required
+#   config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+#   config.fog_public     = false                                   # optional, defaults to true
+#   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
+
+#Set up CarrierWave image uploader
+class ShowImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+  # storage :fog
+  def store_dir
+    "#{Dir.pwd}/public/img/uploads"
+  end
+  def cache_dir
+    "#{Dir.pwd}/public/img/uploads/tmp"
+  end
+  process :resize_to_limit => [720,720]
+  version :thumb do
+    process :resize_to_fill => [60,60]
+  end
+end
+
+####################### DATABASE SETUP ##########################
 
 #Set up Datamapper connection to Postgres (on heroku) or Sqlite on localhost.
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
-
-#Set up Data
 
 class Link
   include DataMapper::Resource
@@ -40,6 +78,9 @@ class Show
   property :description,    Text
   property :created_at,     DateTime
   
+  mount_uploader :image1, ShowImageUploader
+  mount_uploader :image2, ShowImageUploader
+  
   def date_to_s
     self.date_and_time.strftime('%a %d %b %Y')
   end
@@ -50,17 +91,18 @@ class Show
 end
 
 DataMapper.finalize
-
 # DataMapper.auto_migrate! #DROPs table and recreates. Loose all data.
 DataMapper.auto_upgrade! #Tries to change table. Not always cleanly.
 
-
+#################### SETTINGS AND ROUTES ###################
 
 class SpyBoy < Sinatra::Base
   
   configure do
     ENV['ADMIN_USERNAME'] = "bob"
     ENV['ADMIN_PASSWORD'] = "password"
+    
+    enable :sessions
   end
   
   
@@ -155,6 +197,7 @@ class SpyBoy < Sinatra::Base
   post "/show" do
     params[:created_at] = Time.now
     params.delete("submit")
+    
     @show = Show.new(params)
     
     if @show.save
